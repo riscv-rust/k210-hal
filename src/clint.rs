461702cac28442c12965aa57a0b03e4a8978fe05
luojia65 <me@luojia.cc>
2020-05-03 14:14:18 +0800

    Modify CLINT design

--- src/clint.rs
+++ src/clint.rs
@@ -2,59 +2,53 @@
 //!
 //! TODO: Should this module designed in a somehow IP-core peripheral create?
 
-use crate::pac::CLINT;
-
-pub struct Clint {
-    pub msip: [Msip; 2],
-    pub mtime: Mtime,
-    pub mtimecmp: [Mtimecmp; 2],
+/// mtime register
+pub mod mtime {
+    use crate::pac;
+    /// Read mtime register.
+    pub fn read() -> u64 {
+        unsafe { (*pac::CLINT::ptr()).mtime.read().bits() }
+    }
 }
 
-/// Opaque msip register
-pub struct Msip {
-    // false: 0, true: 1
-    // for hals adapting more cores this should be an integer
-    hart_id: bool,
-}
+/// msip register
+///
+/// TODO: not final API design; wait for const generics
+pub mod msip {
+    use crate::pac;
+    // pub fn set_value<const HART_ID: usize>(value: bool) {
+    //     unsafe {
+    //         (*pac::CLINT::ptr()).msip[HART_ID].write(|w| w.bits(if value { 1 } else { 0 }))
+    //     }
+    // }
 
-impl Msip {
-    /// Set msip register value
-    pub fn set_value(&mut self, value: bool) {
-        let hart_id = if self.hart_id { 1 } else { 0 };
+    pub fn set_value(hart_id: usize, value: bool) {
         unsafe {
-            (*CLINT::ptr()).msip[hart_id].write(|w| w.bits(if value { 1 } else { 0 }))
+            (*pac::CLINT::ptr()).msip[hart_id].write(|w| 
+                w.bits(if value { 1 } else { 0 }))
         }
     }
 }
 
-/// Opaque mtimecmp register
-pub struct Mtimecmp {
-    hart_id: bool,
-}
-
-impl Mtimecmp {
-    /// Read mtimecmp register.
-    pub fn mtimecmp(&self) -> u64 {
-        let hart_id = if self.hart_id { 1 } else { 0 };
-        unsafe { (*CLINT::ptr()).mtimecmp[hart_id].read().bits() }
+/// mtimecmp register
+///
+/// TODO: not final API design; wait for const generics
+pub mod mtimecmp {
+    use crate::pac;
+    // pub fn read<const HART_ID: usize>() -> u64 {
+    //     unsafe { (*pac::CLINT::ptr()).mtimecmp[HART_ID].read().bits() }
+    // }
+    // pub fn write<const HART_ID: usize>(value: u64) {
+    //     // Volume II: RISC-V Privileged Architectures V1.10 p.31, figure 3.15
+    //     unsafe { (*pac::CLINT::ptr()).mtimecmp[HART_ID].write(|w| w.bits(value)) };
+    // }
+    pub fn read(hart_id: usize) -> u64 {
+        unsafe { (*pac::CLINT::ptr()).mtimecmp[hart_id].read().bits() }
     }
 
-    /// Write mtimecmp register.
-    pub fn set_mtimecmp(&mut self, value: u64) {
-        let hart_id = if self.hart_id { 1 } else { 0 };
+    pub fn write(hart_id: usize, bits: u64) {
         // Volume II: RISC-V Privileged Architectures V1.10 p.31, figure 3.15
-        unsafe { (*CLINT::ptr()).mtimecmp[hart_id].write(|w| w.bits(value)) };
-    }
-}
-
-/// Opaque mtime register
-pub struct Mtime {
-    _ownership: ()
-}
-
-impl Mtime {
-    /// Read mtime register.
-    pub fn mtime(&self) -> u64 {
-        unsafe { (*CLINT::ptr()).mtime.read().bits() }
+        unsafe { (*pac::CLINT::ptr()).mtimecmp[hart_id].write(|w| 
+            w.bits(bits)) };
     }
 }
