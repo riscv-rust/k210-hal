d955e5423f370083f49ea03ee3785ec2fded1182
Andelf <andelf@gmail.com>
2020-11-18 15:44:48 +0800

    fix: revert embedded-hal to 0.2.4

--- src/gpiohs.rs
+++ src/gpiohs.rs
@@ -3,7 +3,7 @@
 use crate::pac::GPIOHS;
 use core::marker::PhantomData;
 use crate::bit_utils::{u32_set_bit, u32_toggle_bit, u32_bit_is_set, u32_bit_is_clear};
-use embedded_hal::digital::{InputPin, OutputPin};
+use embedded_hal::digital::v2::{InputPin, OutputPin};
 
 // todo: verify
 
@@ -25,7 +25,7 @@ pub trait GpiohsExt {
 
 impl GpiohsExt for GPIOHS {
     fn split(self) -> Parts {
-        Parts { 
+        Parts {
             gpiohs0: Gpiohs0 { _mode: PhantomData },
         }
     }
@@ -117,15 +117,15 @@ impl<MODE> Gpiohs0<MODE> {
 impl<MODE> InputPin for Gpiohs0<Input<MODE>> {
     type Error = core::convert::Infallible;
 
-    fn try_is_high(&self) -> Result<bool, Self::Error> { 
-        Ok(unsafe { 
+    fn is_high(&self) -> Result<bool, Self::Error> {
+        Ok(unsafe {
             let p = &(*GPIOHS::ptr()).input_val as *const _ as *const _;
             u32_bit_is_set(p, 0)
         })
     }
 
-    fn try_is_low(&self) -> Result<bool, Self::Error> { 
-        Ok(unsafe { 
+    fn is_low(&self) -> Result<bool, Self::Error> {
+        Ok(unsafe {
             let p = &(*GPIOHS::ptr()).input_val as *const _ as *const _;
             u32_bit_is_clear(p, 0)
         })
@@ -135,16 +135,16 @@ impl<MODE> InputPin for Gpiohs0<Input<MODE>> {
 impl<MODE> OutputPin for Gpiohs0<Output<MODE>> {
     type Error = core::convert::Infallible;
 
-    fn try_set_high(&mut self) -> Result<(), Self::Error> {
-        unsafe { 
+    fn set_high(&mut self) -> Result<(), Self::Error> {
+        unsafe {
             let p = &(*GPIOHS::ptr()).output_val as *const _ as *mut _;
             u32_set_bit(p, true, 0);
         }
         Ok(())
     }
 
-    fn try_set_low(&mut self) -> Result<(), Self::Error> {
-        unsafe { 
+    fn set_low(&mut self) -> Result<(), Self::Error> {
+        unsafe {
             let p = &(*GPIOHS::ptr()).output_val as *const _ as *mut _;
             u32_set_bit(p, false, 0);
         }
@@ -156,187 +156,187 @@ trait GpiohsAccess {
     fn peripheral() -> &'static mut crate::pac::gpiohs::RegisterBlock;
 
     fn set_drive(index: usize, bit: bool) {
-        unsafe { 
-            let p = &mut Self::peripheral().drive as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().drive as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
 
     fn input_value(index: usize) -> bool {
-        unsafe { 
-            let p = &mut Self::peripheral().input_val as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().input_val as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
 
     fn set_input_en(index: usize, bit: bool) {
-        unsafe { 
-            let p = &mut Self::peripheral().input_en as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().input_en as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
 
     fn set_iof_en(index: usize, bit: bool) {
-        unsafe { 
-            let p = &mut Self::peripheral().iof_en as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().iof_en as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
 
     fn set_iof_sel(index: usize, bit: bool) {
-        unsafe { 
-            let p = &mut Self::peripheral().iof_sel as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().iof_sel as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
 
     fn set_output_en(index: usize, bit: bool) {
-        unsafe { 
-            let p = &mut Self::peripheral().output_en as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().output_en as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
 
     fn set_output_value(index: usize, bit: bool) {
-        unsafe { 
-            let p = &mut Self::peripheral().output_val as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().output_val as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
 
     fn set_output_xor(index: usize, bit: bool) {
-        unsafe { 
-            let p = &mut Self::peripheral().output_xor as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().output_xor as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
 
     fn toggle_pin(index: usize) {
-        unsafe { 
-            let p = &mut Self::peripheral().output_val as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().output_val as *mut _ as *mut _;
             u32_toggle_bit(p, index);
         }
     }
 
     fn set_pullup_en(index: usize, bit: bool) {
-        unsafe { 
-            let p = &mut Self::peripheral().pullup_en as *mut _ as *mut _; 
+        unsafe {
+            let p = &mut Self::peripheral().pullup_en as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
 
     fn set_rise_ie(index: usize, bit: bool) {
         unsafe {
-            let p = &mut Self::peripheral().rise_ie as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().rise_ie as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
-    
+
     fn clear_rise_ip(index: usize) {
         unsafe {
-            let p = &mut Self::peripheral().rise_ip as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().rise_ip as *mut _ as *mut _;
             u32_set_bit(p, true, index);
         }
     }
-    
+
     fn set_fall_ie(index: usize, bit: bool) {
         unsafe {
-            let p = &mut Self::peripheral().fall_ie as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().fall_ie as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
-    
+
     fn clear_fall_ip(index: usize) {
         unsafe {
-            let p = &mut Self::peripheral().fall_ip as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().fall_ip as *mut _ as *mut _;
             u32_set_bit(p, true, index);
         }
     }
-    
+
     fn set_high_ie(index: usize, bit: bool) {
         unsafe {
-            let p = &mut Self::peripheral().high_ie as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().high_ie as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
-    
+
     fn clear_high_ip(index: usize,) {
         unsafe {
-            let p = &mut Self::peripheral().high_ip as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().high_ip as *mut _ as *mut _;
             u32_set_bit(p, true, index);
         }
     }
-    
+
     fn set_low_ie(index: usize, bit: bool) {
         unsafe {
-            let p = &mut Self::peripheral().low_ie as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().low_ie as *mut _ as *mut _;
             u32_set_bit(p, bit, index);
         }
     }
-    
+
     fn clear_low_ip(index: usize) {
         unsafe {
-            let p = &mut Self::peripheral().low_ip as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().low_ip as *mut _ as *mut _;
             u32_set_bit(p, true, index);
         }
     }
 
     fn has_rise_ie(index: usize) -> bool {
         unsafe {
-            let p = &mut Self::peripheral().rise_ie as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().rise_ie as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
 
     fn has_fall_ie(index: usize) -> bool {
         unsafe {
-            let p = &mut Self::peripheral().fall_ie as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().fall_ie as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
 
     fn has_high_ie(index: usize) -> bool {
         unsafe {
-            let p = &mut Self::peripheral().high_ie as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().high_ie as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
 
     fn has_low_ie(index: usize) -> bool {
         unsafe {
-            let p = &mut Self::peripheral().low_ie as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().low_ie as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
 
     fn has_rise_ip(index: usize) -> bool {
         unsafe {
-            let p = &mut Self::peripheral().rise_ip as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().rise_ip as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
 
     fn has_fall_ip(index: usize) -> bool {
         unsafe {
-            let p = &mut Self::peripheral().fall_ip as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().fall_ip as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
 
     fn has_high_ip(index: usize) -> bool {
         unsafe {
-            let p = &mut Self::peripheral().high_ip as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().high_ip as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
 
     fn has_low_ip(index: usize) -> bool {
         unsafe {
-            let p = &mut Self::peripheral().low_ip as *mut _ as *mut _; 
+            let p = &mut Self::peripheral().low_ip as *mut _ as *mut _;
             u32_bit_is_set(p, index)
         }
     }
-    
+
 }
 
 impl GpiohsAccess for GPIOHS {
